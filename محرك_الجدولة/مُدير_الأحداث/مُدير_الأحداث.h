c7eb1df3c8deb57ba85c306b52ae3fefd67a46f5
مطور _العرب <saefhsayn@gmail.com>
2026-07-28 18:04:20 +0300

    Delete محرك_الجدولة directory

--- محرك_الجدولة/مُدير_الأحداث/مُدير_الأحداث.h
+++
@@ -1,8 +0,0 @@
-#ifndef EVENTS_H
-#define EVENTS_H
-
-#define EVENT_SCHEDULE  1
-
-extern volatile int events;
-
-#endif
--- محرك_الجدولة/مُدير_راحة_العمليات/مُدير_راحة_العمليات.h
+++
@@ -1,8 +0,0 @@
-#ifndef SLEEP_H
-#define SLEEP_H
-
-void sleep(int ticks);
-void sleep_ms(int ms);
-
-
-#endif
